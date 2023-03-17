54fefe6b7a83c2e941e15a7c8e973cbed581a24c
lappet69 <sinabariba.andre@gmail.com>
2023-03-17 17:49:32 +0700

    first push

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+const Banner = () => {
+  return (
+    <div>
+      <div>
+        <Image src="" width={100} height={100} alt="" className="cursor-pointer object-contain" />
+      </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/Header.tsx
@@ -0,0 +1,56 @@
+import { AiOutlineBell, AiOutlineSearch } from "react-icons/ai";
+
+import { Barlow_Condensed } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const barlow = Barlow_Condensed({
+  subsets: ["latin"],
+  weight: "400"
+})
+
+const Header = () => {
+  const navMenus = ["Home", "Tv Shows", "Movies", "New & Popular", "My List"]
+
+  const [isScroll, setIsScroll] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScroll(true)
+      } else {
+        setIsScroll(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  return (
+    <header className={`${isScroll && 'bg-red-500'}  `}>
+      <div className={`${barlow.className} flex items-center space-x-2 md:space-x-10`}>
+        <Image src="" width={100} height={50} alt="" className="cursor-pointer object-contain" />
+        <ul className="hidden space-x-4 md:flex">
+          {navMenus.map((menu, idx) => (
+            <li key={idx} className="navLink">{menu}</li>
+          ))}
+        </ul>
+
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm">
+        <AiOutlineSearch className="hidden sm:inline h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <AiOutlineBell className="w-6 h-6" />
+        <Link href={"/account"}>
+          <Image src="" width={24} height={24} alt="" className="cursor-pointer rounded" />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
